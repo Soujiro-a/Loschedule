8dfe9c0c4dc18f1267361f15c0e69d0fc7915acf
Soujiro-a <qmaecx@naver.com>
2022-05-27 12:05:03 +0900

    Setup Security

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import csurf from 'csurf';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
+  app.use(cookieParser());
+  app.use(csurf());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(4000);
 }
