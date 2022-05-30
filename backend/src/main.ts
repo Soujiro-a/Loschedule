b45a2109a3be2110f6618ce0fb848d233ce1b8a3
Soujiro-a <qmaecx@naver.com>
2022-05-30 09:23:05 +0900

    Deleted csurf

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import csurf from 'csurf';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 
@@ -9,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(helmet());
   app.use(cookieParser());
-  app.use(csurf());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(4000);
 }
