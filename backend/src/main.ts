05d9b440ab4cf6e05c319fbf997daab49ebdad4d
Soujiro-a <qmaecx@naver.com>
2022-06-07 16:50:47 +0900

    Config KST timezone

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
+import moment from 'moment-timezone';
+import { KOREA_TIMEZONE } from './common/common.constants';
 
 async function bootstrap() {
+  moment.tz.setDefault(KOREA_TIMEZONE);
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(helmet());
   app.use(cookieParser());
