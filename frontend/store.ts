fa7ba08c295ec1402f7bebb0825d899b4a9d6b19
Soujiro-a <qmaecx@naver.com>
2022-07-03 22:04:18 +0900

    Fixed redux store config

--- frontend/store.ts
+++ frontend/store.ts
@@ -1,8 +1,19 @@
-import { configureStore, EnhancedStore, Reducer } from "@reduxjs/toolkit";
-import { createWrapper } from "next-redux-wrapper";
-import rootReducer from "./slices";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import { AnyAction, configureStore, Reducer } from "@reduxjs/toolkit";
+import { createWrapper, MakeStore } from "next-redux-wrapper";
+import rootReducer, { IState } from "./slices";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "./storage";
+import LocalStorage from "./class/LocalStorage";
+import { X_JWT } from "./constants";
 
 const devMode = process.env.NODE_ENV === "development";
 
@@ -16,21 +27,34 @@ export const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const makeConfiguredStore = (reducer: Reducer) =>
   configureStore({
-    reducer,
-    middleware: [],
+    reducer: reducer as Reducer<IState, AnyAction>,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+        thunk: {
+          extraArgument: { jwt: LocalStorage.getItem(X_JWT) },
+        },
+      }),
     devTools: devMode,
   });
+
+let store = makeConfiguredStore(persistedReducer);
+
 const makeStore = () => {
   const isServer = typeof window === "undefined";
 
   if (isServer) {
     return makeConfiguredStore(rootReducer);
   } else {
-    const store = makeConfiguredStore(persistedReducer);
-    let persistor = persistStore(store);
+    const persistor = persistStore(store);
     return { persistor, ...store };
   }
 };
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export const wrapper = createWrapper(makeStore, {
   debug: devMode,
 });
