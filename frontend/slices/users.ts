0ab5e0b37bb0b14f92080d36d0df0db1123084f8
Soujiro-a <qmaecx@naver.com>
2022-07-05 13:45:15 +0900

    Fixed login, logout action case

--- frontend/slices/users.ts
+++ frontend/slices/users.ts
@@ -1,6 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { IState } from ".";
 import { logInAction, logOutAction } from "../actions/users";
+import LocalStorage from "../class/LocalStorage";
 import { IUser } from "../interface/user";
+import { PERSIST_ROOT, X_JWT } from "../constants";
 const initialState: IUser = {
   isLoggedIn: false,
   user: null,
@@ -15,11 +18,23 @@ export const users = createSlice({
       .addCase(logInAction.fulfilled, (state, action) => {
         state.user = action.payload;
         state.isLoggedIn = true;
+        LocalStorage.setItem(X_JWT, action.payload!.token!);
       })
-      .addCase(logInAction.rejected, (state, action) => {})
+      .addCase(logInAction.rejected, (state, action) => {
+        state.isLoggedIn = false;
+      })
       .addCase(logOutAction.pending, (state, action) => {})
-      .addCase(logOutAction.fulfilled, (state, action) => {})
+      .addCase(logOutAction.fulfilled, (state, action) => {
+        state.user = null;
+        state.isLoggedIn = false;
+        LocalStorage.removeItem(X_JWT);
+        LocalStorage.removeItem(PERSIST_ROOT);
+      })
       .addCase(logOutAction.rejected, (state, action) => {})
       .addDefaultCase(() => {}),
 });
-export default users.reducer;
+
+export const isLoggedInSelector = (state: IState) => state.users.isLoggedIn;
+export const userSelector = (state: IState) => state.users.user;
+
+export default users;
