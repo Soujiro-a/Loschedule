eef184d663ad06960d0ecd61ae72c6ff6716d677
Soujiro-a <qmaecx@naver.com>
2022-07-09 00:03:56 +0900

    Fixed useMe hooks

--- frontend/hooks/useMe.tsx
+++ frontend/hooks/useMe.tsx
@@ -1,11 +1,36 @@
-export const useMe = async (token: string) => {
-  const BACKEND_URL = process.env.BACKEND_URL;
-  const user = await (
-    await fetch(`${BACKEND_URL}/user`, {
-      headers: {
-        "x-jwt": token,
-      },
-    })
-  ).json();
-  return user;
+import axios from "axios";
+import Router from "next/router";
+import { useEffect } from "react";
+import LocalStorage from "../class/LocalStorage";
+import { X_JWT } from "../constants";
+import { isLoggedInSelector, userSelector } from "../slices/users";
+import { useAppSelector } from "./useAppSelector";
+
+export const useMe = ({ redirectTo = "/login" }: { redirectTo?: string }) => {
+  const isLoggedIn = useAppSelector(isLoggedInSelector);
+  const loggedInUser = useAppSelector(userSelector);
+  const token = LocalStorage.getItem(X_JWT);
+
+  useEffect(() => {
+    (async () => {
+      if (isLoggedIn) return;
+
+      if (token) {
+        const user = await axios.get(`${process.env.backendUrl}/user`, {
+          headers: {
+            "x-jwt": token,
+          },
+        });
+
+        if (!user) {
+          Router.push(redirectTo);
+        } else {
+          return user;
+        }
+      } else {
+        Router.push(redirectTo);
+      }
+    })();
+  });
+  return loggedInUser;
 };
