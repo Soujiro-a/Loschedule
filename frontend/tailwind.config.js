2624a9b5e2fc16086b2fcb584cb35b3cf0e71c1c
Soujiro-a <qmaecx@naver.com>
2022-07-16 10:38:40 +0900

    Config flowbite

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/tw-elements/dist/js/**/*.js'
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('tw-elements/dist/plugin')
+    require('flowbite/plugin')
   ],
 }
