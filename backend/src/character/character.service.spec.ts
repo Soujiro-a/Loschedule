8206bbc8349b6ee003b7ce4dab3e981bde7cee14
Soujiro-a <qmaecx@naver.com>
2022-07-17 11:16:09 +0900

    Create character service test

--- backend/src/character/character.service.spec.ts
+++ backend/src/character/character.service.spec.ts
@@ -1,12 +1,24 @@
+import { HttpModule } from '@nestjs/axios';
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { mockRepository } from 'src/common/test.constants';
+import { CrawlerService } from 'src/crawler/crawler.service';
+import { User } from 'src/user/schemas/user.schema';
 import { CharacterService } from './character.service';
+import { Character } from './schemas/character.schema';
 
 describe('CharacterService', () => {
   let service: CharacterService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CharacterService],
+      imports: [HttpModule],
+      providers: [
+        CharacterService,
+        CrawlerService,
+        { provide: getModelToken(User.name), useValue: mockRepository },
+        { provide: getModelToken(Character.name), useValue: mockRepository },
+      ],
     }).compile();
 
     service = module.get<CharacterService>(CharacterService);
