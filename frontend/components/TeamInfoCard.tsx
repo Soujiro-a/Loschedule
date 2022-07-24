ff15803afb2af23ce99ffc558c2aa81b2962325a
Soujiro-a <qmaecx@naver.com>
2022-07-24 14:25:31 +0900

    Create TeamInfoCard component

---
+++ frontend/components/TeamInfoCard.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import React from "react";
+import Button from "./Button";
+
+interface ITeamInfoProps {
+  teamName: string;
+  memberCount: number;
+  teamId: string;
+}
+
+const TeamInfoCard: React.FC<ITeamInfoProps> = ({
+  memberCount,
+  teamName,
+  teamId,
+}) => {
+  return (
+    <div className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {teamName}
+      </h5>
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        memberCount : {memberCount}
+      </p>
+      <Link href={`/team/${teamId}`}>
+        <a className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          팀 바로가기
+          <svg
+            className="ml-2 -mr-1 w-4 h-4"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </a>
+      </Link>
+    </div>
+  );
+};
+
+export default TeamInfoCard;
