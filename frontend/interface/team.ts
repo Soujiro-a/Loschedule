4e794c4e46819bd0ab93380b5cf32e1670e00bb2
Soujiro-a <qmaecx@naver.com>
2022-07-28 09:25:06 +0900

    Create Team interface

--- frontend/interface/team.ts
+++ frontend/interface/team.ts
@@ -1,1 +1,21 @@
-export interface ITeam {}
+export interface ITeam {
+  _id: string;
+  name: string;
+  leader: string;
+  members: string[];
+  raids: string[];
+  __v: number;
+}
+
+export interface ITeamInfoProps {
+  members: string[];
+  memberCount: number;
+  teamName: string;
+  teamId: string;
+  raids: string[];
+  leader: string;
+}
+
+export interface ICreateTeamForm {
+  teamName: string;
+}
