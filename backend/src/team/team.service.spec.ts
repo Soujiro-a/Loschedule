2264c5af44339b6862353769d6f3b652e2ee6084
Soujiro-a <qmaecx@naver.com>
2022-08-06 00:53:19 +0900

    Create team service test case

--- backend/src/team/team.service.spec.ts
+++ backend/src/team/team.service.spec.ts
@@ -23,4 +23,67 @@ describe('TeamService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('create', () => {
+    it.todo('팀 생성 성공');
+    describe('팀 생성 실패', () => {
+      it.todo('예기치 못한 오류');
+    });
+  });
+
+  describe('delete', () => {
+    it.todo('팀 삭제 성공');
+    describe('팀 삭제 실패', () => {
+      it.todo('존재하지 않는 팀');
+      it.todo('팀 삭제를 시도한 사람이 팀 리더가 아님');
+      it.todo('예기치 못한 오류');
+    });
+  });
+
+  describe('join', () => {
+    it.todo('팀 가입 성공');
+    describe('팀 가입 실패', () => {
+      it.todo('존재하지 않는 팀');
+      it.todo('이미 소속되어있는 팀');
+      it.todo('예기치 못한 오류');
+    });
+  });
+
+  describe('leave', () => {
+    it.todo('팀 탈퇴 성공');
+    describe('팀 탈퇴 실패', () => {
+      it.todo('존재하지 않는 팀');
+      it.todo('팀 리더일 경우(팀 해체만 가능)');
+      it.todo('소속되어있지 않은 팀에 대한 탈퇴 요청');
+      it.todo('예기치 못한 오류');
+    });
+  });
+
+  describe('changeLeader', () => {
+    it.todo('팀 리더 변경 성공');
+    describe('팀 리더 변경 실패', () => {
+      it.todo('존재하지 않는 팀');
+      it.todo('팀 리더 변경을 시도한 사람이 팀 리더가 아님');
+      it.todo('팀 멤버가 아닌 사람에게 팀 리더를 주려고 함');
+      it.todo('예기치 못한 오류');
+    });
+  });
+
+  describe('getRaids', () => {
+    it.todo('팀 내 레이드 일정 정보 조회 성공');
+    describe('팀 내 레이드 일정 정보 조회 실패', () => {
+      it.todo('존재하지 않는 팀');
+      it.todo('팀 멤버가 아닌 경우');
+      it.todo('예기치 못한 오류');
+    });
+  });
+
+  describe('getMembers', () => {
+    it.todo('팀 내 멤버 정보 조회 성공');
+    describe('팀 내 멤버 정보 조회 실패', () => {
+      it.todo('존재하지 않는 팀');
+      it.todo('팀 멤버가 아닌 경우');
+      it.todo('예기치 못한 오류');
+    });
+  });
 });
