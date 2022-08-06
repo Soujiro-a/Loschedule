4633b8b1819c7404ec01903010934e2940cb98fc
Soujiro-a <qmaecx@naver.com>
2022-08-07 00:21:48 +0900

    Create profile page

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -1,9 +1,19 @@
+import { GetServerSideProps } from "next";
 import Title from "../components/Title";
+import { useAppSelector } from "../hooks/useAppSelector";
+import { isLoggedInSelector } from "../slices/users";
+import wrapper from "../store";
+import { useMe } from "../hooks/useMe";
+import { OutputLoginUserType } from "../interface/user";
 
 export default function Profile() {
+  const isLoggedIn = useAppSelector(isLoggedInSelector);
+  const user: OutputLoginUserType = useMe({});
   return (
     <div>
       <Title title="Profile" />
+      {isLoggedIn ? "logged!" : "not logged!"}
+      <div>{user && user.nickname}</div>
     </div>
   );
 }
