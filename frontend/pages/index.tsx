84914c2fce5d9a8b26bf9b578d130653208b146e
Soujiro-a <qmaecx@naver.com>
2022-08-13 10:50:43 +0900

    Fixed index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,37 +1,15 @@
-import {
-  GetServerSideProps,
-  GetStaticProps,
-  InferGetServerSidePropsType,
-} from "next";
-import { logInAction } from "../actions/users";
 import Title from "../components/Title";
-import { useAppDispatch } from "../hooks/useAppDispatch";
 import { useAppSelector } from "../hooks/useAppSelector";
-import { useMe } from "../hooks/useMe";
-import { IState } from "../slices";
-import wrapper from "../store";
+import { isLoggedInSelector } from "../slices/users";
 
-export default function Home({
-  user,
-}: InferGetServerSidePropsType<GetServerSideProps>) {
-  const isLoggedIn = useAppSelector((state: IState) => state.users.isLoggedIn);
+export default function Home() {
+  const isLoggedIn = useAppSelector(isLoggedInSelector);
   return (
-    <div>
+    <>
       <Title title="Home" />
-    </div>
+      <div>
+        <div>{isLoggedIn ? "logged!" : "not logged!"}</div>
+      </div>
+    </>
   );
 }
-
-// 사용 예시
-export const getServerSideProps: GetServerSideProps =
-  wrapper.getServerSideProps((store) => async () => {
-    // const user = await useMe(
-    //   "" // 서버 로그인을 통해 발급받은 jwt토큰 값
-    // );
-    return {
-      props: {
-        value: 1,
-        // user,
-      },
-    };
-  });
