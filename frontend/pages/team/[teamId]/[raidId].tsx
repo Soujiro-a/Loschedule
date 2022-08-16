15d32cf931613ff19223b472e7905a149b19ee9a
Soujiro-a <qmaecx@naver.com>
2022-08-16 12:16:33 +0900

    Create test case in raid page

--- frontend/pages/team/[teamId]/[raidId].tsx
+++ frontend/pages/team/[teamId]/[raidId].tsx
@@ -1,6 +1,17 @@
 import Title from "../../../components/Title";
 
 export default function Raid() {
+  const testRaidData = {
+    leader: {
+      nickname: "leader",
+    },
+    bossName: "boss",
+    targetDate: "2022-06-12 22:15",
+    characters: [
+      { name: "코마호", level: 1571, job: "창술사", server: "카마인" },
+      { name: "코다치", level: 140, job: "리퍼", server: "카마인" },
+    ],
+  };
   return (
     <div>
       <Title title="Raid" />
