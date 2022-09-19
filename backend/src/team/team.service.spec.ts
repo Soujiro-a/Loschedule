9afd8e71ff7f660a775e5bf009550a0a8c6ec322
Soujiro-a <qmaecx@naver.com>
2022-09-20 00:17:48 +0900

    Create team getMembers function fail test case

--- backend/src/team/team.service.spec.ts
+++ backend/src/team/team.service.spec.ts
@@ -161,9 +161,17 @@ describe('TeamService', () => {
   describe('getMembers', () => {
     it.todo('팀 내 멤버 정보 조회 성공');
     describe('팀 내 멤버 정보 조회 실패', () => {
+      const mockTeamId = 'test';
       it.todo('존재하지 않는 팀');
       it.todo('팀 멤버가 아닌 경우');
-      it.todo('예기치 못한 오류');
+      it('예기치 못한 오류', async () => {
+        const result = await service.getMembers(user, mockTeamId);
+
+        expect(result).toMatchObject({
+          ok: false,
+          error: '팀 멤버 정보들을 불러올 수 없습니다.',
+        });
+      });
     });
   });
 });
